f0b78506bf13500da5bcce26bb3023583c5cfe7e
gaowq <1138946467@qq.com>
2021-07-26 23:01:47 +0800

    初始化

---
+++ DataAccess/AlanContext .cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using wms.Entities;
+
+namespace wms.DataAccess
+{
+    public class AlanContext:DbContext
+    {
+        public AlanContext(DbContextOptions<AlanContext> options)
+           : base(options)
+        { }
+
+        public DbSet<Classify> Classify { get; set; }
+    }
+}
---
+++ DataAccess/Interface/IAlanDao.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using wms.Entities;
+
+namespace wms.DataAccess.Interface
+{
+    public interface IAlanDao
+    {
+        bool CreateClassify(Classify classify);
+
+        ////插入数据
+        //bool CreateStudent(Student student);
+
+        ////取全部记录
+        //IEnumerable<Student> GetStudents();
+
+        ////取某id记录
+        //Student GetStudentByID(int id);
+
+        ////根据id更新整条记录
+        //bool UpdateStudent(Student student);
+
+        ////根据id更新名称
+        //bool UpdateNameByID(int id, string name);
+
+        ////根据id删掉记录
+        //bool DeleteStudentByID(int id);
+    }
+}
